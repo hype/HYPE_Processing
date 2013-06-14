278e03c270619e5e0d03010e0457042ed47b0e56
james <james@snmini>
2013-06-14 18:09:53 +0800

    update documentation

--- doc/html/classhype_1_1extended_1_1drawable_1_1_h_rect.js
+++ doc/html/classhype_1_1extended_1_1drawable_1_1_h_rect.js
@@ -8,6 +8,7 @@ var classhype_1_1extended_1_1drawable_1_1_h_rect =
     [ "draw", "classhype_1_1extended_1_1drawable_1_1_h_rect.html#ac616395ca715c8fb6dddf74b203df51d", null ],
     [ "rounding", "classhype_1_1extended_1_1drawable_1_1_h_rect.html#aee2ab04d2ff50abe87c0622aa870bd88", null ],
     [ "rounding", "classhype_1_1extended_1_1drawable_1_1_h_rect.html#ade308b69a8b6ac1c03d8e0e5f2dcc0ee", null ],
+    [ "rounding", "classhype_1_1extended_1_1drawable_1_1_h_rect.html#a0a111fc3f6117886c14f9c4e24269b2d", null ],
     [ "roundingBL", "classhype_1_1extended_1_1drawable_1_1_h_rect.html#ad66438698f1bd3a33b214f3041c979b3", null ],
     [ "roundingBL", "classhype_1_1extended_1_1drawable_1_1_h_rect.html#acfddc39e1df383904a9c15317dae6d07", null ],
     [ "roundingBR", "classhype_1_1extended_1_1drawable_1_1_h_rect.html#a403cf9af3529a2251f9ac9316a668bab", null ],
@@ -15,6 +16,5 @@ var classhype_1_1extended_1_1drawable_1_1_h_rect =
     [ "roundingTL", "classhype_1_1extended_1_1drawable_1_1_h_rect.html#a3dbebef6e54ba3c87be895d9e47fd8c4", null ],
     [ "roundingTL", "classhype_1_1extended_1_1drawable_1_1_h_rect.html#ac949ee8c38d5f35d3f440ea000406632", null ],
     [ "roundingTR", "classhype_1_1extended_1_1drawable_1_1_h_rect.html#aee3e7b8de8f70b3476ec0257312e8e35", null ],
-    [ "roundingTR", "classhype_1_1extended_1_1drawable_1_1_h_rect.html#a4512a97bf07ae5d008c00274dea1a8b0", null ],
-    [ "_tl", "classhype_1_1extended_1_1drawable_1_1_h_rect.html#ad5ef226a32749be17c4ef83cf1a55f1e", null ]
+    [ "roundingTR", "classhype_1_1extended_1_1drawable_1_1_h_rect.html#a4512a97bf07ae5d008c00274dea1a8b0", null ]
 ];
