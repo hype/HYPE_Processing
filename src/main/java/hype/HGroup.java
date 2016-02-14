04b296f7edcc9fdcc17b33db04868d34e74ae986
DonoG <DonoG@users.noreply.github.com>
2016-02-15 10:26:11 +1100

    Update HGroup.java
    Update so HGroup can have rotations on X, Y and Z axis

--- src/main/java/hype/HGroup.java
+++ src/main/java/hype/HGroup.java
@@ -1,40 +1,63 @@
-package hype;
+ /*
+ *
+ * HYPE_Processing
+ * http://www.hypeframework.org/ & https://github.com/hype/HYPE_Processing
+ * 
+ * Copyright (c) 2013-2015 Joshua Davis & James Cruz
+ * 
+ * Distributed under the BSD License. See LICENSE.txt for details.
+ * 
+ * All rights reserved.
+ *
+ * ----- *
+ * HGroup - updated by GD on 26/1/16 to enable X,Y and Z rotations *
+ * Tested using processing 3.0.1 (It may break on newer versions of processing) *
+ * If you find any issues or make anything cool with this, let me know on twitter at @Garth_D *
+ */
 
+package hype;
 import processing.core.PGraphics;
 
 public class HGroup extends HDrawable {
 
-	public HGroup() {
-		transformsChildren(true).stylesChildren(true);
-	}
+  public HGroup() {
+    transformsChildren(true).stylesChildren(true);
+  }
 
-	@Override
-	public HGroup createCopy() {
-		HGroup copy = new HGroup();
-		copy.copyPropertiesFrom(this);
-		return copy;
-	}
+  @Override
+  public HGroup createCopy() {
+    HGroup copy = new HGroup();
+    copy.copyPropertiesFrom(this);
+    return copy;
+  }
 
-	@Override
-	public void paintAll(PGraphics g, boolean usesZ, float alphaPc) {
-		if(this.alphaPc <=0) return;
+ @Override
+  public void paintAll(PGraphics g, boolean usesZ, float alphaPc) {
+    if (alphaPc<=0) return;
+ 
+// Perform a trimmed down version of super.paintAll()
+   g.pushMatrix();
 
-		// Perform a trimmed down version of super.paintAll()
-		g.pushMatrix();
-			if(usesZ) g.translate(x, y, z);
-			else g.translate(x, y);
-			g.rotate(rotationZRad);
+    if (usesZ) {
+      g.translate(x, y, z);
+      g.rotateX(rotationXRad);
+      g.rotateY(rotationYRad);
+      g.rotateZ(rotationZRad);
+    } else {
+      g.translate(x, y);
+      g.rotate(rotationZRad);
+    }
 
-			alphaPc *= this.alphaPc;
+    alphaPc *= alphaPc;
+    HDrawable child = firstChild;
+    while (child != null) {
+      child.paintAll(g, usesZ, alphaPc);
+      child = child.next();
+    }
+    g.popMatrix();
+  }
 
-			HDrawable child = firstChild;
-			while(child != null) {
-				child.paintAll(g, usesZ, alphaPc);
-				child = child.next();
-			}
-		g.popMatrix();
-	}
-
-	@Override
-	public void draw(PGraphics g,boolean b,float x,float y,float f) {}
+  @Override
+  public void draw(PGraphics g, boolean b, float x, float y, float f) {
+  }
 }
