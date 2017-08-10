faaea1d1ca641aa40d67087beff5633d0027896e
Benjamin Fox <ben@tracerstar.com>
2017-08-09 23:38:01 -0700

    Adding Z property to HFollow. Now follow in 3D space.

--- src/main/java/hype/extended/behavior/HFollow.java
+++ src/main/java/hype/extended/behavior/HFollow.java
@@ -7,7 +7,7 @@ import hype.HMouse;
 import processing.core.PApplet;
 
 public class HFollow extends HBehavior {
-	private float ease, spring, dx, dy;
+	private float ease, spring, dx, dy, dz;
 	private HLocatable goal;
 	private HLocatable follower;
 
@@ -89,9 +89,11 @@ public class HFollow extends HBehavior {
 
 		dx = dx*spring + (goal.x()-follower.x()) * ease;
 		dy = dy*spring + (goal.y()-follower.y()) * ease;
+		dz = dz*spring + (goal.z()-follower.z()) * ease;
 
 		follower.x(follower.x() + dx);
 		follower.y(follower.y() + dy);
+		follower.z(follower.z() + dz);
 	}
 
 	@Override
