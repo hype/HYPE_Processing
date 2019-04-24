9fd7552f89e8e50d8f9138d30b485f3680e5ccd4
nacho cossio <ignaciojcossio@gmail.com>
2019-04-24 18:25:20 +0200

    added class to handle per drawable shader

---
+++ src/main/java/hype/HMaterial.java
@@ -0,0 +1,48 @@
+package hype;
+
+import javax.swing.text.StyledEditorKit.BoldAction;
+
+import processing.core.PApplet;
+import processing.core.PGraphics;
+import processing.opengl.PShader;
+
+public class HMaterial {
+	PShader shader;
+	boolean active = true;
+	
+	public HMaterial(){
+		shader = null;
+	}
+	
+	public HMaterial(HMaterial copy){
+		shader = copy.shader;
+	}
+	
+	public void bind(PGraphics pg){
+		if(!active)
+			return;
+		if(shader != null)
+		{
+			pg.shader(shader);
+		}
+	}
+	
+	public boolean getActive() {
+		return active;
+	}
+
+	public HMaterial setActive(boolean _active) {
+		this.active = _active;
+		return this;
+	}
+	public PShader getShader() {
+		return shader;
+	}
+
+	public HMaterial setShader(PShader shader) {
+		this.shader = shader;
+		return this;
+	}
+	
+	
+}
