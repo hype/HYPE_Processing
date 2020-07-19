5cfec284edae22ab8be91d8b2ac2f6f247759f71
PastParticle <pastparticle@github.com>
2020-07-19 16:45:05 -0700

    Updated HColorPool to not repeat random colors when calling getColor()

--- src/main/java/hype/extended/colorist/HColorPool.java
+++ src/main/java/hype/extended/colorist/HColorPool.java
@@ -6,10 +6,12 @@ import hype.HMath;
 import hype.interfaces.HColorist;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class HColorPool implements HColorist {
 	private ArrayList<Integer> colorList;
-	private boolean fillFlag, strokeFlag;
+	private ArrayList<Integer> colorStack;
+	private boolean fillFlag, strokeFlag, repeatColors = true;
 
 	public HColorPool(int... colors) {
 		colorList = new ArrayList<Integer>();
@@ -22,10 +24,15 @@ public class HColorPool implements HColorist {
 		HColorPool copy = new HColorPool();
 		copy.fillFlag = fillFlag;
 		copy.strokeFlag = strokeFlag;
+		copy.repeatColors = repeatColors;
 
 		for(int i=0; i< colorList.size(); ++i) {
 			int clr = colorList.get(i);
 			copy.colorList.add( clr );
+		}
+		for(int i=0; i< colorStack.size(); ++i) {
+			int clr = colorStack.get(i);
+			copy.colorStack.add( clr );
 		}
 		return copy;
 	}
@@ -36,19 +43,28 @@ public class HColorPool implements HColorist {
 
 	public HColorPool add(int clr) {
 		colorList.add(clr);
+		resetColorStack();
 		return this;
 	}
 
 	public HColorPool add(int clr, int freq) {
 		while(freq-- > 0) colorList.add(clr);
+		resetColorStack();
 		return this;
 	}
 
 	public int getColor() {
 		if(colorList.size() <= 0) return 0;
 
-		int index = (int) Math.floor(H.app().random(colorList.size()));
-		return colorList.get(index);
+		if (repeatColors) {
+			int index = (int) Math.floor(H.app().random(colorList.size()));
+			return colorList.get(index);
+		} else {
+			if (colorStack.size() == 0) {
+				resetColorStack();
+			}
+			return colorStack.remove(0);
+		}
 	}
 
 	public int getColor(int seed) {
@@ -59,8 +75,31 @@ public class HColorPool implements HColorist {
 	}
 
 	public int getColorAt(int index) {
-		if(colorList.size() <= index) return 0;
+		if (colorList.size() <= index) return 0;
 		return colorList.get(index);
+	}
+
+	/**
+	 * If set to false, HColorPool will cycle through colors entirely before choosing the same color.
+	 *
+	 * @return HColorPool
+	 */
+	public HColorPool repeatColors(boolean repeatColors) {
+		this.repeatColors = repeatColors;
+		return this;
+	}
+
+	/**
+	 * Resets the color stack - used if randomColors() is called on an HShape, making sure when colorizing a
+	 * shape that it starts with all available colors before removing them.
+	 *
+	 * @return HColorPool
+	 */
+	public HColorPool resetColorStack() {
+		colorStack = new ArrayList<Integer>();
+		colorStack.addAll(colorList);
+		Collections.shuffle(colorStack);
+		return this;
 	}
 
 	@Override
