7269129a497a7023fb1dc31906c00a708c5fc123
Benjamin Fox <hello@benjaminfoxstudios.com>
2023-08-03 20:21:44 -0700

    adding draw() method to HCanvas for consistency across HDrawable subclasses

--- HYPE/src/main/java/hype/HCanvas.java
+++ HYPE/src/main/java/hype/HCanvas.java
@@ -268,6 +268,98 @@ public class HCanvas extends HDrawable {
 		return this;
 	}
 
+	public void draw(PGraphics g) {
+		if(this.alphaPc <=0 || width ==0 || height ==0) return;
+
+		boolean zFlag = false;
+		if( g.is3D() ) zFlag = true;
+
+		g.pushMatrix();
+			// Rotate and translate
+			if(zFlag) g.translate(x, y, z);
+			else g.translate(x, y);
+			g.rotate(rotationZRad);
+
+			// Compute current alpha
+			alphaPc *= this.alphaPc;
+
+			// Initialize the buffer
+			graphics.beginDraw();
+
+			// Prepare the buffer for this frame
+			if(autoClear) {
+				graphics.clear();
+			} else {
+				if(hasFilter) {
+					if(hasFilterParam) graphics.filter(filterKind, filterParam);
+					else graphics.filter(filterKind);
+				}
+				if(hasFade) {
+					if(!renderer.equals(PConstants.JAVA2D))
+						graphics.loadPixels();
+
+					int[] pix = graphics.pixels;
+					for(int i=0; i<pix.length; ++i) {
+						int clr = pix[i];
+						int a = clr >>> 24;
+						if(a == 0) continue;
+						a -= fadeAmt;
+						if(a < 0) a = 0;
+						pix[i] = clr & 0xFFFFFF | (a << 24);
+					}
+					graphics.updatePixels();
+				}
+				if(hasBlend) {
+					graphics.blend(
+						0,0, graphics.width, graphics.height,
+						0,0, graphics.width, graphics.height, blendMode);
+				}
+			}
+
+			//handle lights
+			if (useLights == false) {
+				graphics.noLights();
+			} else {
+				//check if there are any HLights
+				if (hasLights) {
+					for(HLight l : lights) {
+						switch (l.type()) {
+							case 1:
+								graphics.pointLight(l.v1, l.v2, l.v3, l.x, l.y, l.z);
+								break;
+							case 2:
+								graphics.directionalLight(l.v1, l.v2, l.v3, l.x, l.y, l.z);
+								break;
+						}
+					}
+				} else {
+					graphics.lights();
+				}
+			}
+
+			// Draw children
+			HDrawable child = firstChild;
+			while(child != null) {
+				child.paintAll(graphics, usesZ(), alphaPc);
+				child = child.next();
+			}
+
+			if (hasShader) {
+				for(PShader s : shader) {
+					graphics.filter(s);
+				}
+			}
+
+			// Finalize the buffer
+			graphics.endDraw();
+
+			// Draw the buffer
+			g.image(graphics,0,0);
+		g.popMatrix();
+	}
+
+
+
 	@Override
 	public void paintAll(PGraphics g, boolean zFlag, float alphaPc) {
 		if(this.alphaPc <=0 || width ==0 || height ==0) return;
